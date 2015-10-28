d86f2ba890af168047eb3d41513b4dfd9ec613ef
lnspham <lnspham@gmail.com>
2015-10-28 11:31:14 -0700

    Add in parseInt & display F within the if condition

--- js/index.js
+++ js/index.js
@@ -4,32 +4,34 @@ $(document).ready(function(){
 	function convertTemp() {
 
 		/*identify the variables*/
-		var c = $('#celsius').val(); //Celsius. Get user input
+		var c = parseInt($('#celsius').val()); //Celsius. Get user input
 		var f= (c * 9)/5 + 32;; //converting Celsius to Fahrenheit 
 		
-		/*call for actions*/
-		$('#fahreneit').html('ahhh')
 
 		/*provide the actions*/
 		if (f <= 32) {
 
 			$('body').css('background-image', 'url("images/winter.jpg")');
+			$('#fahreneit').html(f);
 
 		}if else (f > 32 && f <= 60) {
 
 			$('body').css('background-image', 'url("images/fall.jpg")');
+			$('#fahreneit').html(f);
 
 		}if else (f > 60 && f <= 75) {
 
 			$('body').css('background-image', 'url("images/spring.jpg")');
+			$('#fahreneit').html(f);
 
 		}else {
 
 			$('body').css('background-image', 'url("images/winter.jpg")');
-
+			$('#fahreneit').html(f);
 		}
 	}
 
+	/*call for actions*/
 	$('#convert').click(convertTemp);
 
 });
